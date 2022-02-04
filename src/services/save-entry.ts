7f401e03d1e77a6b8cba1532e4d7238b2871ac7e
Adrien Grandemange <itsmeadri@gmail.com>
2022-02-04 23:48:27 +0100

    Patch wav entries when downloading so they are readable on modern media players

--- src/services/save-entry.ts
+++ src/services/save-entry.ts
@@ -9,12 +9,20 @@ export function saveEntry(
   metadata: EntryMetadata | undefined,
   dataTypes: DataTypes | null
 ) {
-  const blob = new Blob([entry.content], { type: 'application/octet-stream' });
-  const name = `${filename}_${index}`;
+  let content = entry.content;
   let extension = 'dat';
   if (metadata && dataTypes) {
     extension = dataTypes[metadata.game][metadata.type].extension;
+    if (extension === 'wav') {
+      // Patch .wav entries so they can be played on modern media players.
+      content = new ArrayBuffer(entry.content.byteLength);
+      const view = new Uint8Array(content);
+      view.set(new Uint8Array(entry.content));
+      view[0] = 0x52;
+    }
   }
+  const blob = new Blob([content], { type: 'application/octet-stream' });
+  const name = `${filename}_${index}`;
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   saveAs(blob, `${name}.${extension}`);
 }
